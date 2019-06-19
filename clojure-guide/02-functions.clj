ddaa5fdcc35a2ba356631100220a1c14791d483a
Ivan Echevarria <ivan@echevarria.io>
2019-06-19 08:46:10 -0700

    Add solutions to functions problems

--- clojure-guide/02-functions.clj
+++ clojure-guide/02-functions.clj
@@ -69,3 +69,81 @@
 
 (defn triplicate [f] (f) (f) (f))
 
+
+
+;; 8) Define a function opposite which takes a single argument f. It should
+;;      return another function which takes any number of arguments, applies f
+;;      on them, and then calls not on the result. The not function in Clojure
+;;      does logical negation.
+
+(defn opposite [f]
+  (fn ([& args] (not (apply f args)))))
+
+
+
+;; 9) Define a function triplicate2 which takes another function and any number
+;;      of arguments, then calls that function three times on those arguments.
+;;      Re-use the function you defined in the earlier triplicate exercise.
+
+(defn triplicate2 [f & args]
+  (triplicate (fn 
+    ([] (f))
+    ([args] (f args)))))
+
+
+
+;; 10) Using the java.lang.Math class (Math/pow, Math/cos, Math/sin, Math/PI), 
+;;      demonstrate the following mathematical facts:
+;;        - The cosine of pi is -1
+;;        - For some x, sin(x)^2 + cos(x)^2 = 1
+
+(assert (= (Math/cos Math/PI) -1.0))
+
+(defn square [number] (* number number))
+(def half_pi (/ Math/PI 2))
+(assert 
+  (= 
+    (+(square (Math/sin half_pi))
+      (square (Math/cos half_pi))) 
+    1.0))
+
+
+
+;; 11) Define a function that takes an HTTP URL as a string, fetches that URL
+;;      from the web, and returns the content as a string.
+
+;; Hint: Using the java.net.URL class and its openStream method. Then use the
+;; Clojure slurp function to get the content as a string.
+
+(import (java.net URL))
+(defn http-get [url]
+  (slurp (.openStream (URL. url))))
+
+(assert (.contains (http-get "https://www.echevarria.io") "html"))
+
+;; In fact, the Clojure slurp function interprets its argument as a URL first
+;; before trying it as a file name. Write a simplified http-get:
+
+(defn http-get [url]
+  (slurp url))
+
+
+
+;; 12) Define a function one-less-arg that takes two arguments:
+;;      - f, a function
+;;      - x, a value
+;;      and returns another function which calls f on x plus any additional
+;;      arguments.
+
+(defn one-less-arg [f x]
+  (fn [& args] (apply f x args)))
+
+
+
+;; 13) Define a function two-fns which takes two functions as arguments, f and
+;;      g. It returns another function which takes one argument, calls g on it,
+;;      then calls f on the result, and returns that. That is, your function 
+;;      returns the composition of f and g.
+
+(defn two-fns [f g]
+  (fn [x] (f (g x))))
